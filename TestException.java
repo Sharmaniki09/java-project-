0f3eed9e821442cf2446c69867e119218891d601
Sharmaniki09 <109590122+Sharmaniki09@users.noreply.github.com>
2022-08-21 03:47:17 -0400

    Add files via upload

---
+++ TestException.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.lang.Exception;
+
+public class TestException {
+
+	public static void main(String[] args)
+	{
+		Scanner Sc = new Scanner(System.in);
+		{
+			System.out.println("enter two no");
+			String s1=Sc.nextLine();
+			String s2=Sc.nextLine();
+			try
+			{
+				int a=Integer.parseInt(s1);
+			    int b=Integer.parseInt(s2);
+			    int c=a/b;
+			    System.out.println("divided by zero a&b:"+c);
+			    
+			}
+			catch(NumberFormatException ef)
+			{
+				System.out.println("format mismatch");
+			}
+			catch(Exception e)
+			{
+				System.out.println("invalid exception"+e);
+			}
+			finally
+			{
+				System.out.println("exception caught");
+			}
+		}
+		System.out.println("exception handled");
+	}
+}
