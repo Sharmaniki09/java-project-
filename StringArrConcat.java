8f0d5930c98d267b8a97cbb16f9df47c2452f4f3
Sharmaniki09 <109590122+Sharmaniki09@users.noreply.github.com>
2022-08-25 08:50:41 -0400

    Add files via upload

---
+++ StringArrConcat.java
@@ -0,0 +1,36 @@
+
+public class StringArrConcat {
+	// accepting a String array from user
+	static String strConcat(String st[])
+	{
+	// converting st array int StringBuilder object, for using append method
+	StringBuilder builder = new StringBuilder();;
+	try {
+	// throwing an exception when st is empty
+	if(st==null) {
+	throw new Exception();
+	}
+	// join array values one by one using for each loop
+	for(String s : st) {
+	// append string one by on at the end of builder obj
+	builder.append(s+" ");
+	}
+	}catch(Exception e)
+	{
+	// this will print a msg when string is null
+	System.out.print("Array Cannot Null");
+	}
+	// returning in the form of String
+	return builder.toString();
+	}
+	public static void main(String[] args) {
+	// make three array of Strings
+	String st1[]= null;
+	String st2[]= {"How","Are","You"};
+	String st3[]= {"Nikita","Sharma"};
+	// Invoking method three times strConcat(strarr)
+	System.out.println(strConcat(st1));
+	System.out.println(strConcat(st2));
+	System.out.println(strConcat(st3));
+}
+}
