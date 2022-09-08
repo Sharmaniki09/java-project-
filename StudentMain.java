831ea9acc3df1c5a63e185a50b1a64d552ebb73c
Sharmaniki09 <109590122+Sharmaniki09@users.noreply.github.com>
2022-09-08 17:38:43 +0530

    Add files via upload

---
+++ StudentMain.java
@@ -0,0 +1,53 @@
+package mypack;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student obj1 =new Student();
+		{
+		obj1.setinstitute_name (" Anudip Foundation");
+		obj1.setRoll_No(123);
+		obj1.setName("Nikita");
+		obj1.setAge(20);
+		
+	
+		System.out.println(obj1.getRoll_No());
+		System.out.println(obj1.getName());
+		System.out.println(obj1.getAge());
+		
+		}
+		
+		
+		Student obj2 =new Student();
+		{	
+		obj2.setinstitute_name (" Anudip Foundation");
+		obj2.setRoll_No(124);
+		obj2.setName("Komal");
+		obj2.setAge(22);
+		
+		
+		System.out.println(obj2.getRoll_No());
+		System.out.println(obj2.getName());
+		System.out.println(obj2.getAge());
+		}
+		
+		
+		Student obj3 =new Student();
+		{
+		obj3.setinstitute_name (" Anudip Foundation");
+		obj3.setRoll_No(125);
+		obj3.setName("Niharika");
+		obj3.setAge(21);
+		
+		
+		System.out.println(obj3.getRoll_No());
+		System.out.println(obj3.getName());
+		System.out.println(obj3.getAge());
+		}
+	}
+
+
+
+	}
+
+
