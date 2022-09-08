726d359c645335e47d0a0164aa75f19dda9eb421
Sharmaniki09 <109590122+Sharmaniki09@users.noreply.github.com>
2022-09-08 17:36:44 +0530

    Add files via upload

---
+++ InterfaceTest.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class InterfaceTest {
+
+	public static void main(String[] args) {
+		Square s=new Square();
+		System.out.println("Area of Square is="+s.area(4, 4));
+		Rectangle r=new Rectangle();
+		System.out.println("Area of Rectangle is="+r.area(4, 2));
+
+	}
+
+}
