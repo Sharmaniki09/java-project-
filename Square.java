c74fd8fe09343ea2e16f77f2ea6b104f52eee406
Sharmaniki09 <109590122+Sharmaniki09@users.noreply.github.com>
2022-09-08 17:38:02 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+package mypack;
+
+public class Square implements Shape{
+	int length;
+	//override the method
+
+	public int area(int x, int y) {
+		//TODO Auto-generated method stub
+		return x*y;
+	}
+
+	}
+
+
